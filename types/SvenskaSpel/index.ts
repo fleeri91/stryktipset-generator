d90311c43b0ddf005ed6a503a17b3bc5b1e9ba2a
Flemming Eriksson <fleeri91@gmail.com>
2026-02-08 21:22:54 +0100

    export fif

---
+++ types/SvenskaSpel/index.ts
@@ -0,0 +1,4 @@
+export type { EventRoot, Event } from './Event'
+export type { ResultRoot, Result } from './Result'
+export type { ResultsHistoryRoot } from './ResultsHistory'
+export { EventType } from './EventType'
